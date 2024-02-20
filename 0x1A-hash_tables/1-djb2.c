79e3f7674ef926c8f968fc1bdad125d3aff36331
theeacademic <nyawira22.kelly@student.cuk.ac.ke>
2024-02-19 23:27:54 -0800

    djb2 algo

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,11 +1,15 @@
+/**
+ * hash_djb2 - implements the djb2 algorithm
+ * @str: string used to generate hash value
+ *
+ * Return: hash value
+ */
 unsigned long int hash_djb2(const unsigned char *str)
 {
-    unsigned long int hash = 5381;
-    int c;
-
-    while ((c = *str++))
-        hash = ((hash << 5) + hash) + c;
-
-    return hash;
+unsigned long int hash = 5381;
+int c;
+while ((c = *str++))
+hash = ((hash << 5) + hash) +c;
+return (hash);
 }
 
